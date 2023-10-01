8191c149b6e6c2f73f97ae8ee5f96c7d19161e02
Everlyn <gachuieve32@gmail.com>
2023-10-01 21:11:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+document.addEventListener('DOMContentLoaded',fetchBooks);
 
-function renderBooks(books) {
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((json) => {
+      renderBooks(json);
+    });
+    
+}function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
@@ -12,6 +16,3 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
